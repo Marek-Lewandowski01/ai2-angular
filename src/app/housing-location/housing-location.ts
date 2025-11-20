573f4216ad91ee3f7ef90a255018a0f5eb6da6c1
Marek Lewandowski <lm51031@zut.edu.pl>
2025-11-20 15:28:07 +0100

    add housing location component and styles

---
+++ src/app/housing-location/housing-location.ts
@@ -0,0 +1,26 @@
+import {Component, input} from '@angular/core';
+import {HousingLocationInfo} from '../housinglocation';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [
+    NgOptimizedImage
+  ],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+    </section>
+  `,
+  styleUrl: './housing-location.css',
+})
+export class HousingLocation {
+  housingLocation = input.required<HousingLocationInfo>();
+}
